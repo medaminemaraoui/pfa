5f02127bd1c4b59e6d03f8bc25759819a63fc99d
medaminemaraoui <aminemaroui145@gmail.com>
2025-01-15 00:23:54 +0100

    field-service with angular worked

--- Fields-Service/src/main/java/org/sid/fieldsservice/repository/FieldRepository.java
+++ Fields-Service/src/main/java/org/sid/fieldsservice/repository/FieldRepository.java
@@ -2,11 +2,13 @@ package org.sid.fieldsservice.repository;
 
 import org.sid.fieldsservice.entity.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+//@Repository
+@RepositoryRestResource
 public interface FieldRepository extends JpaRepository<Field, Long> {
     List<Field> findByLocation(String location);
 
--- Gateway-Service/src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
+++ Gateway-Service/src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -3,6 +3,10 @@ package org.sid.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient // Optional, only needed if using Eureka
@@ -11,6 +15,13 @@ public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator DynamicRoutes(ReactiveDiscoveryClient discoveryClient,
+                                                               DiscoveryLocatorProperties properties) {
+
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
     }
 
 }
